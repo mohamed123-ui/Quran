8a8f2b32b98f6e8f035df4784ec1d03223f798d5
mohamed123-ui <slymmhmdahmd548@gmail.com>
2025-06-21 20:47:00 +0300

    lastchange

--- src/app/Client-Layout.tsx
+++ src/app/Client-Layout.tsx
@@ -1,12 +1,10 @@
 "use client";
-import { Provider } from "react-redux";
 import { Store } from "./Redux/Store";
 import Navbar from "./Components/Navbar";
 import Footer from "./Components/Footer";
-
+import { Provider } from "react-redux";
 export default function ClientLayout({ children }: { children: React.ReactNode }) {
   return (
-
       <Provider store={Store}>
         <div className="h-[10vh]">
           <Navbar />
